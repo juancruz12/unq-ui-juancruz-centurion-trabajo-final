26a3f9bfcc75858727e92b2eeb71237e1ff7b8bd
juanCruz12 <juan_century@hotmail.com>
2021-07-10 18:53:24 -0300

    throw all no funciona

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
 import { useEffect,useState } from "react";
 import React from "react";
 import ReactDOM from 'react-dom';
-const App = () => {
-  const [text, setText] = useState("hola")
-  const [count, setCount] = useState(0);
 
-  useEffect(() =>{
-
-    if(text=="hola") return; 
-    setCount(count+1);
-
-  },[text]);
-  return (
-    <div>
-      <p>Componente {count} - {text}</p>
-      <button onClick={() => setText("texto1")}>texto1</button>
-      <button onClick={() => setText("texto2")}>texto2</button>
-      <button onClick={() => setText("texto3")}>texto3</button>
-      <button onClick={() => setText("texto4")}>texto4</button>
-    </div>
-  )
-}
-export default App;
 class Square extends React.Component {
   constructor(props){
     super(props);
@@ -30,23 +10,42 @@ class Square extends React.Component {
       value: null,
     };
   }
+  changeNumber = () => {
+    this.setState({value: Math.floor(Math.random() * 6) + 1});
+  } 
   render() {
     return (
+      <div>
       <button className="square"
-        onClick={() => this.setState({value: "X"})}>
+        onClick={() => this.changeNumber()}>
         {this.state.value}
       </button>
+      <button className="throw"
+      onClick={() => this.changeNumber()}>
+        Throw this
+      </button>
+      </div>
     );
   }
 }
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: []
+    };
+  }
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square value={this.state.squares[i]}/>;
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Puntaje:';
 
+  const throwAll = () =>{
+      this.state.squares.foreach (dice => (dice.changeNumber()))
+    }
+  
     return (
       <div>
         <div className="status">{status}</div>
@@ -54,22 +53,19 @@ class Board extends React.Component {
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
           {this.renderSquare(3)}
           {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          <button onClick ={()=>this.state.squares.foreach(dice => dice.changeNumber()) }>
+            Throw all
+          </button>
         </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        
       </div>
     );
   }
 }
-class Game extends React.Component {
+
+class App extends React.Component {
   render() {
     return (
       <div className="game">
@@ -79,108 +75,15 @@ class Game extends React.Component {
         <div className="game-info">
          <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
-       </div>
-    </div>
-);
+        </div>     
+        </div>
+   );
   }
 }
+export default App
+
+
 ReactDOM.render(
-  <Game />,
+  <App />,
   document.getElementById('root')
 );
-/*
-import {React}
- class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-      key: null,
-      value: null,
-    };
-  }
-  render() {
-    return (
-      <button className="square"
-        onClick={() => this.setState({value: "X"})}>
-        {this.state.value}
-      </button>
-    );
-  }
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i}/>;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-         <div>{/* status *///}</div>
-//          <ol>{/* TODO */}</ol>
- //       </div>
-//     </div>
-//);
-//  }
-//}
-//ReactDOM.render(
-  //<Game />,
- // document.getElementById('root')
-//);
-//export default App;
-/////////////////////////////////////////////////////////////////////////
-/*import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;*/
